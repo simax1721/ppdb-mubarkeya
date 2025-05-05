b71a53ac13371eccb8310e4af88aadf78f1e1742
simax1721 <simax1721@gmail.com>
2025-05-05 20:39:05 +0700

    hapus link /public/

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -108,47 +108,51 @@
     <div class="container">
 
       <div class="section-title">
-        <h2>Visi dan Misi Sekolah</h2>
-        <p class="" style="font-style: italic">"Mewujudkan Generasi SMK Unggul, terampil, berbudaya yang berkarakter edutechnopreuneur islami serta menjunjung tinggi nilai Pancasila dalam menghadapi Era Digital"</p>
+        <h2>Visi</h2>
+        <p class="" style="font-style: italic">"Mewujudkan Generasi SMK Unggul, terampil, berbudaya yang berkarakter edutechnopreuneur islami <br> serta menjunjung tinggi nilai Pancasila dalam menghadapi Era Digital"</p>
+      </div>
+      <div class="section-title">
+        <h2>Misi</h2>
+        {{-- <p class="" style="font-style: italic">"Mewujudkan Generasi SMK Unggul, terampil, berbudaya yang berkarakter edutechnopreuneur islami serta menjunjung tinggi nilai Pancasila dalam menghadapi Era Digital"</p> --}}
       </div>
 
-      <div class="row no-gutters">
+      <div class="row no-gutters justify-content-center">
 
-        <div class="col-lg-4 col-md-6 content-item" data-aos="fade-up">
+        <div class="col-lg-4 col-md-6 content-item" data-aos="fade-up" style="border-left: 0px">
           <span>01</span>
-          <h4>Lorem Ipsum</h4>
-          <p>Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et consectetur ducimus vero placeat</p>
+          {{-- <h4>Lorem Ipsum</h4> --}}
+          <p>Mendidik peserta didik yang beriman dan bertaqwa kepada Tuhan Yang Maha Esa, berakhlak mulia, berkebhinnekaan global, mandiri, bergotong royong, bernalar kritis, kreatif.</p>
         </div>
 
         <div class="col-lg-4 col-md-6 content-item" data-aos="fade-up" data-aos-delay="100">
           <span>02</span>
-          <h4>Repellat Nihil</h4>
-          <p>Dolorem est fugiat occaecati voluptate velit esse. Dicta veritatis dolor quod et vel dire leno para dest</p>
+          {{-- <h4>Repellat Nihil</h4> --}}
+          <p>Menyelenggarakan dan melaksanakan proses pembelajaran yang ditangani langsung oleh tenaga profesional melalui perkembangan teknologi sesuai program keahliannya dengan pendekatan kearifan lokal.</p>
         </div>
 
         <div class="col-lg-4 col-md-6 content-item" data-aos="fade-up" data-aos-delay="200">
           <span>03</span>
-          <h4> Ad ad velit qui</h4>
-          <p>Molestiae officiis omnis illo asperiores. Aut doloribus vitae sunt debitis quo vel nam quis</p>
+          {{-- <h4> Ad ad velit qui</h4> --}}
+          <p>Melatih peserta didik yang berkarakter dan berjiwa wirausaha.</p>
         </div>
 
-        <div class="col-lg-4 col-md-6 content-item" data-aos="fade-up" data-aos-delay="300">
+        <div class="col-lg-4 col-md-6 content-item" data-aos="fade-up" data-aos-delay="300" style="border-left: 0px">
           <span>04</span>
-          <h4>Repellendus molestiae</h4>
-          <p>Inventore quo sint a sint rerum. Distinctio blanditiis deserunt quod soluta quod nam mider lando casa</p>
+          {{-- <h4>Repellendus molestiae</h4> --}}
+          <p>Menerapkan budaya literasi, penguasaan perkembangan teknologi informasi, dalam memahami keberagaman sosial budaya.</p>
         </div>
 
         <div class="col-lg-4 col-md-6 content-item" data-aos="fade-up" data-aos-delay="400">
           <span>05</span>
-          <h4>Sapiente Magnam</h4>
-          <p>Vitae dolorem in deleniti ipsum omnis tempore voluptatem. Qui possimus est repellendus est quibusdam</p>
+          {{-- <h4>Sapiente Magnam</h4> --}}
+          <p>Melatih peserta didik melalui pola kemitraan dengan dunia usaha dan industri menjadi tenaga kerja yang handal serta mampu bersaing secara global.</p>
         </div>
 
-        <div class="col-lg-4 col-md-6 content-item" data-aos="fade-up" data-aos-delay="500">
+        {{-- <div class="col-lg-4 col-md-6 content-item" data-aos="fade-up" data-aos-delay="500">
           <span>06</span>
-          <h4>Facilis Impedit</h4>
+          {{-- <h4>Facilis Impedit</h4>
           <p>Quis eum numquam veniam ea voluptatibus voluptas. Excepturi aut nostrum repudiandae voluptatibus corporis sequi</p>
-        </div>
+        </div> --}}
 
       </div>
 
